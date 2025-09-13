dd515d90b2ab0540e269fe12c22f249781eb41dd
tombuckelew <tom.buckelew1@verizon.net>
2025-09-13 05:04:57 -0500

    Add GitHub Desktop helper script

---
+++ open_in_github_desktop.bat
@@ -0,0 +1,10 @@
+@echo off
+echo Opening Stock Predictor in GitHub Desktop...
+start "" "github://openRepo/https://github.com/YOUR_USERNAME/stock-predictor"
+echo.
+echo If GitHub Desktop doesn't open:
+echo 1. Open GitHub Desktop manually
+echo 2. File -^> Add Local Repository
+echo 3. Browse to: C:\Projects\Predictor
+echo 4. Click "Add Repository"
+pause
